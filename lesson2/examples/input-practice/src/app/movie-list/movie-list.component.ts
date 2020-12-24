796d4b5b138224e2cf0fc542b58600694e2e1ac7
chad <canddx3@gmail.com>
2020-12-24 13:22:16 -0600

    first commit

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+   errorMessage = 'no space';
 
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie(newTitle: string) {
+      if(newTitle === '') {
+         console.log(this.errorMessage);
+      } else if(!this.movies.includes(newTitle)) {
+         this.movies.push(newTitle);
+      }
    }
 }
